9b3a1fa3643744d708e78a8e4daa8ada8356fc82
truc0 <22969604+truc0@users.noreply.github.com>
2023-07-15 22:11:10 +0800

    chore: remove unused code and rename some variable

--- main.cpp
+++ main.cpp
@@ -4,34 +4,13 @@
 
 const unsigned N = 2048;
 
-// std::vector<std::complex<float>, RealAllocatorType> compute_sycl(unsigned N, sycl::queue &queue, std::vector<float, RealAllocatorType> src)
-// {
-//     oneapi::mkl::dft::descriptor<oneapi::mkl::dft::precision::SINGLE,
-//                                  oneapi::mkl::dft::domain::REAL>
-//         desc(N);
-
-//     desc.set_value(oneapi::mkl::dft::config_param::CONJUGATE_EVEN_STORAGE, DFTI_COMPLEX_COMPLEX);
-//     desc.set_value(oneapi::mkl::dft::config_param::PLACEMENT, DFTI_NOT_INPLACE);
-//     desc.set_value(oneapi::mkl::dft::config_param::NUMBER_OF_TRANSFORMS, 1);
-//     desc.commit(queue);
-
-//     std::vector<std::complex<float>, RealAllocatorType> dst(N);
-
-//     auto event = oneapi::mkl::dft::compute_forward<
-//         decltype(desc), float, std::complex<float>>(desc, src.data(), dst.data());
-//     event.wait();
-
-//     return dst;
-// }
-
 int main()
 {
     auto generator = Generator();
-    auto srcReal = generator.generate2DRandomArray(N);
-    // auto srcImag = generator.generate2DZeroArray(N);
+    auto input = generator.generate2DRandomArray(N);
 
     FFTExecutor *executor = new OneMKLFFTExecutor();
-    auto dst = executor->execute(N, srcReal, generator.queue());
+    auto dst = executor->execute(N, input, generator.queue());
 
     for (const auto &it : dst)
     {
